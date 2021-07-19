78d077f45db1108462231d1268f2717690926077
virginiacc <vczosek@gmail.com>
2021-07-19 14:48:26 -0700

    Rename app target and add spacing.

--- src/Filters.js
+++ src/Filters.js
@@ -2,17 +2,23 @@ import Filter from "./Filter.js";
 
 function Filters( props ) { 
   return (
-    <div className="filters o-well">
+    <div className="filters o-well block block__sub">
       <h3>Find rental assistance programs for...</h3>
       <Filter handler={ props.setState }
               options={ props.stateOptions }
               label='Your state or territory'
               placeholder='Select your state or territory'/>
-      <Filter options={ props.tribeOptions }
-              handler={ props.setTribe }
-              label='Your tribe or Tribally Designated Housing Entity (if applicable)'
-              placeholder='Select your tribe or TDHE'
-              helperText='Only tribes and TDHEs with rental assistance programs will be listed.' />
+      <div className="block 
+                      block__sub 
+                      block__border-top 
+                      block__padded-top 
+                      block__flush-bottom">
+        <Filter options={ props.tribeOptions }
+                handler={ props.setTribe }
+                label='Your tribe or Tribally Designated Housing Entity (if applicable)'
+                placeholder='Select your tribe or TDHE'
+                helperText='Only tribes and TDHEs with rental assistance programs will be listed.' />
+      </div>
     </div>
   );
 }

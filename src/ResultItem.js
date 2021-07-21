8e5ca2f3b98d68f0c2d7e18abe3de6e3ff2bf13b
virginiacc <vczosek@gmail.com>
2021-07-21 12:00:57 -0700

    Refine results (#3)
    * Add icons, explanation, and links to Notification component
    * Add fields to ResultsItem
    * Add tests for utility functions
    * Add "unlisted" option for tribe filter
    * Add custom notifications for "all programs", "filtered results", and "no results" scenarios
    * Detect urls in program contact info

--- src/ResultItem.js
+++ src/ResultItem.js
@@ -1,7 +1,45 @@
 const ResultItem = ( props ) => {
+  let contact = props.item[ 'Program Page Link (Phone # if Link is Unavailable)' ];
+  if ( contact.startsWith( 'http' ) ) {
+    contact = <a href="{ contact }" rel="noreferrer" target="_blank">{ contact }</a>
+  }
   return (
     <div className="block block__sub block__border-bottom block__padded-bottom">
-      <h2>{ props.item['Program Name'] }</h2>
+      <h3>
+        { props.item[ 'City/County/Locality' ] ||
+          props.item[ 'Tribal Government/Territory' ] ||
+          props.item[ 'State' ]
+        }
+      </h3>
+      <dl>
+        { props.item[ 'Geographic Level' ] !== 'Tribal Government' &&
+          <div>
+            <dt>State/Territory:</dt>
+            <dd>
+              { props.item[ 'State' ] || 
+                props.item[ 'Tribal Government/Territory' ] }
+            </dd>
+          </div>
+        }
+        <div>
+          <dt>Program name:</dt>
+          <dd>
+            { props.item[ 'Program Name' ] }
+          </dd>
+        </div>
+        <div>
+          <dt>Program type:</dt>
+          <dd>
+            { props.item[ 'Geographic Level' ] }
+          </dd>
+        </div>
+        <div>
+          <dt>Get started:&nbsp;</dt>
+          <dd>
+            { contact }
+          </dd>
+        </div>
+      </dl>
     </div>    
   )
 };
---
+++ src/__tests__/utils.test.js
@@ -0,0 +1,168 @@
+import { 
+  filterGeographicPrograms,
+  filterTribalPrograms,
+  generateTribalOptions, 
+  processData 
+} from '../utils.js';
+
+const tribalPrograms = [
+	{
+    "Geographic Level": "Tribal Government",
+    "State": "Oklahoma",
+    "Tribal Government/Territory": "Apache Tribe of Oklahoma",
+    "Program Name": "Apache Tribe of Oklahoma Emergency Rental Assistance Program (ERAP)",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://apachetribe.org/emergency-rental-assistance-program-erap/",
+  },
+  {
+    "Geographic Level": "Tribal Government",
+    "State": "Oklahoma",
+    "City/County/Locality": "",
+    "Tribal Government/Territory": "Caddo Nation",
+    "Program Name": "Caddo Nation Emergency Rental Assistance",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://mycaddonation.com/housing",
+  },
+  {
+    "Geographic Level": "Tribal Government",
+    "State": "Oklahoma",
+    "City/County/Locality": "",
+    "Tribal Government/Territory": "Cherokee Nation",
+    "Program Name": "Cherokee Nation Emergency Rental Assistance Program (ERAP)",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://www.hacn.org/hacn",
+  }
+]
+
+const geographicPrograms = [
+   {
+    "Geographic Level": "State",
+    "State": "Alabama",
+    "City/County/Locality": "",
+    "Program Name": "Alabama Emergency Rental Assistance Program",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://eraalabama.com/",
+  },
+  {
+    "Geographic Level": "County",
+    "State": "Alabama",
+    "City/County/Locality": "Baldwin County",
+    "Program Name": "Baldwin County Emergency Rental Assistance Program",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://www.baldwinaltogether.org/",
+  },
+  {
+    "Geographic Level": "City",
+    "State": "Alabama",
+    "City/County/Locality": "Birmingham",
+    "Program Name": "Birmingham Emergency Rental Assistance Program",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://www.birminghamal.gov/renthelp",
+  },
+  {
+    "Geographic Level": "State",
+    "State": "California",
+    "City/County/Locality": "",
+    "Tribal Government/Territory": "",
+    "Program Name": "California's COVID-19 Rent Relief",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://landlordtenant.dre.ca.gov/covid_rr/index.html",
+  },
+  {
+    "Geographic Level": "County",
+    "State": "California",
+    "City/County/Locality": "Alameda County",
+    "Tribal Government/Territory": "",
+    "Program Name": "Alameda County Housing Secure Emergency Rental Assistance Program (ACHS-ERAP)",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://www.ac-housingsecure.org/",
+  },
+  {
+    "Geographic Level": "City",
+    "State": "California",
+    "City/County/Locality": "Anaheim",
+    "Tribal Government/Territory": "",
+    "Program Name": "Anaheim Emergency Rental Assistance Program",
+    "Program Page Link (Phone # if Link is Unavailable)": "https://www.anaheim.net/5532/Emergency-Rental-Assistance-Program",
+  }
+]
+
+const allPrograms = [...geographicPrograms, ...tribalPrograms]
+
+describe('module::utils', () => {
+
+  describe( 'filterTribalPrograms', () => {
+    it( 'returns result for tribe selection' , () => {
+      const results = filterTribalPrograms( 
+        tribalPrograms, 'Oklahoma', 'Caddo Nation'
+      )
+      expect( results.length ).toEqual( 1 );
+      expect( results[0] ).toEqual( tribalPrograms[1] )
+    } );
+
+    it( 'returns no results for unmatched tribe selection' , () => {
+      const results = filterTribalPrograms( 
+        tribalPrograms, 'Oklahoma', 'Nation'
+      )
+      expect( results ).toEqual( [] );
+      expect( results.length ).toEqual( 0 );
+    } );
+
+    it( 'returns no results when state but not tribe is selected' , () => {
+      const results = filterTribalPrograms( 
+        tribalPrograms, 'California', ''
+      )
+      expect( results.length ).toEqual( 0 );
+    } );
+
+    it( 'returns no results if tribe value equals null' , () => {
+      const results = filterTribalPrograms( 
+        tribalPrograms, '', null
+      )
+      expect( results.length ).toEqual( 0 );
+    } );
+
+    it( 'returns all results if no state or tribe is selected' , () => {
+      const results = filterTribalPrograms( 
+        tribalPrograms, '', ''
+      )
+      expect( results.length ).toEqual( 3 );
+      expect( results[0] ).toEqual( tribalPrograms[0] )
+    } );
+  } );
+
+  describe( 'filterGeographicPrograms', () => {
+    it( 'returns results for state selection' , () => {
+      const results = filterGeographicPrograms( 
+        geographicPrograms, 'California', 'Caddo Nation'
+      )
+      expect( results.length ).toEqual( 3 );
+      expect( results[0] ).toEqual( geographicPrograms[3] )
+    } );
+
+    it( 'returns no results when tribe but not state is selected' , () => {
+      const results = filterGeographicPrograms( 
+        geographicPrograms, '', 'Caddo Nation'
+      )
+      expect( results.length ).toEqual( 0 );
+    } );
+
+    it( 'returns all results when neither tribe nor state is selected' , () => {
+      const results = filterGeographicPrograms( 
+        geographicPrograms, '', ''
+      )
+      expect( results ).toEqual( geographicPrograms );
+    } );
+
+  } );
+
+  describe( 'generateTribalOptions', () => {
+    it( 'generates options for all tribes and an unlisted option' , () => {
+      const options = generateTribalOptions( tribalPrograms );
+      expect( options.length ).toEqual( 4 );
+      expect( options[0].value ).toEqual( null )
+      expect( options[1].value ).toEqual( tribalPrograms[0]["Tribal Government/Territory"] )
+    } );
+  } );
+
+  describe( 'processData', () => {
+    it( 'separates tribal and geographic programs' , () => {
+      const [ geographic, tribal ] = processData( allPrograms );
+      expect( geographic ).toEqual( geographicPrograms );
+      expect( tribal ).toEqual( tribalPrograms );
+    } );
+  } );
+
+})

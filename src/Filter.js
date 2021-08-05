7c01b551353d80a64297193ba974e40ddafed999
virginiacc <vczosek@gmail.com>
2021-08-04 19:50:21 -0700

    Customize react-select styles.

--- src/Filter.js
+++ src/Filter.js
@@ -1,5 +1,23 @@
 import Select from 'react-select';
 
+const customStyles = {
+  control: base => ( {
+    ...base,
+    borderColor: '#919395',
+    borderRadius: 0
+  } ),
+  dropdownIndicator: base => ( {
+    ...base,
+    backgroundColor: '#e7e8e9',
+    color: '#101820 !important'
+  } ),
+  indicatorSeparator: base => ( {
+    ...base,
+    backgroundColor: '#919395',
+    margin: 0
+  } )
+};
+
 const Filter = ( props ) => {
   return (
     <div className="m-form-field">
@@ -20,6 +38,7 @@ const Filter = ( props ) => {
               onChange={ props.onChange }
               options={ props.options }
               placeholder={ props.placeholder }
+              styles={customStyles}
               value={ [ props.value ] }/>
       </div>
     );

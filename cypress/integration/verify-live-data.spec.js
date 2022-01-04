c43e31a6626d52bc40a0d8c6e773f5ba6346ba7b
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-01-04 12:52:03 -0500

    Fix cypress testing logic for when states don't have statewide programs

--- cypress/integration/verify-live-data.spec.js
+++ cypress/integration/verify-live-data.spec.js
@@ -5,36 +5,39 @@ describe( 'Test results against live data', () => {
   before( () => {
     cy.visit( 'http://localhost:3000/' );
     cy.waitForReact();
-  });
+  } );
 
-  const [ stateData, countyData ] = processData( programData );
+  const [ stateData, countyData, statewidePrograms ] = processData( programData );
   const states = Object.keys( stateData );
   states.forEach( state => {
     const statePrograms = stateData[state];
     const count = statePrograms.length;
-    const vals = statePrograms.map( program => program.name).join('; ');
-    it(`should show ${count} program${count > 1 ? "s" : ""} for ${state} -- (${vals})`, () => {
+    const statewideProgramCount = Number( Boolean( statewidePrograms[state] ) );
+    const vals = statePrograms.map( program => program.name ).join( '; ' );
+    it( `should show ${ count } program${ count > 1 ? 's' : '' } for ${ state } -- (${ vals })`, () => {
       selectOption( 'state-select', state );
-      cy.get('.result-item').should( 'have.length', count );
-    });
+      cy.get( '.result-item' ).should( 'have.length', count );
+    } );
     const programsByCounty = countyData[state];
     if ( programsByCounty ) {
       const counties = Object.keys( programsByCounty );
       counties.forEach( county => {
         const countyPrograms = programsByCounty[county];
-        const count = countyPrograms.length + 1;
-        const vals = countyPrograms.join('; ');
-        it(`should show ${count} programs for ${county}, ${state} -- (${vals}; ${state} state)`, () => {
+        const count = countyPrograms.length + statewideProgramCount;
+        const vals = countyPrograms.join( '; ' );
+        it( `should show ${ count } programs for ${ county }, ${ state } -- (${ vals }; ${ state } state)`, () => {
           selectOption( 'state-select', state );
           selectOption( 'county-select', county );
-          cy.get('.result-item').should( 'have.length', count );
-          cy.get('.result-item h3').contains( state ).should( 'exist' );
+          cy.get( '.result-item' ).should( 'have.length', count );
+          if ( statewideProgramCount ) {
+            cy.get( '.result-item h3' ).contains( state ).should( 'exist' );
+          }
           countyPrograms.forEach( program => {
-            cy.get('.result-item h3').contains( program ).should( 'exist' );
-          })
-        })
-      })
+            cy.get( '.result-item h3' ).contains( program ).should( 'exist' );
+          } );
+        } );
+      } );
     }
   } );
 
-});
+} );
